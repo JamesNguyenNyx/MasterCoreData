75553b6a270afba460c4651fcc0efa8609b09853
James Nguyen <jamesnguyen.nyx@gmail.com>
2017-08-21 21:31:38 +0700

    Initializer Tuberose

---
+++ Tuberose/Tuberose/AppDelegate/AppDelegate.swift
@@ -0,0 +1,63 @@
+//
+//  AppDelegate.swift
+//  Tuberose
+//
+//  Created by James Nguyen on 2017/08/21.
+//  Copyright © 2017 James Nguyen. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        self.configureLogin()
+        return true
+    }
+    
+    
+}
+
+
+//MARK: FilePrivate AppDelegate
+fileprivate extension AppDelegate {
+    
+    fileprivate func isLogin() -> Bool {
+        return false
+    }
+    
+    fileprivate func configureLogin() {
+        if isLogin() {
+            
+        } else {
+            
+        }
+    }
+    
+    fileprivate func configureRootViewController() {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = self.window {
+            let mainViewController = MainViewController(nibName: MainViewController.className, bundle: nil)
+            let navigation = BaseNavigationController(rootViewController: mainViewController)
+            window.rootViewController = navigation
+            window.makeKeyAndVisible()
+        }
+    }
+    
+    fileprivate func setRootAuthenticationViewController() {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = self.window {
+            let mainViewController = MainViewController(nibName: MainViewController.className, bundle: nil)
+            let navigation = BaseNavigationController(rootViewController: mainViewController)
+            window.rootViewController = navigation
+            window.makeKeyAndVisible()
+        }
+        
+    }
+}
+
---
+++ Tuberose/Tuberose/Controller/MainViewController/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  Tuberose
+//
+//  Created by James Nguyen on 2017/08/21.
+//  Copyright © 2017 James Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
